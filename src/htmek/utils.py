8f6f5e754d89df0019778a13ac26c9c8e9d619f7
micah-olivas <micah5051olivas@gmail.com>
2025-04-02 14:47:27 -0700

    Revert "Merge branch 'main' of https://github.com/FordyceLab/htmek"
    This reverts commit 297022e9854baf2d276452afa0ab0d143beec1c9, reversing
    changes made to 4c3dd326bbe05526cea06259f58e7437ba28d416.

--- src/htmek/utils.py
+++ src/htmek/utils.py
@@ -2,14 +2,11 @@ import numpy as np
 from skimage.transform import rescale
 import tifffile as tf
 from pathlib import Path
-import magnify
 
 import magnify
 
 
-def bin2x2(
-    array: np.array,
-) -> np.array:
+def bin2x2(array: np.array) -> np.array:
     """Takes an unbinned image and performs a correction to match the
     2x2 binning performed by the camera."""
 
@@ -22,9 +19,6 @@ def bin2x2(
 
     # Scale intensity by difference in image size
     array_2x2 = array_2x2 * (x / new_x) * (y / new_y)
-
-    # Adjust to 16-bit int
-    array_2x2 = np.clip(array_2x2, 0, 65535).astype('uint16')
 
     return array_2x2
 
@@ -51,6 +45,5 @@ def make_binned_tiff(
     array = array.astype('int16')
 
     tf.imwrite(output, array)
-    
-    if verbose:
-        print(f'File written to {output}.')
+
+    print(f'File written to {output}.')
